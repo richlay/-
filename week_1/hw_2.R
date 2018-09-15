2075ad3cdb3df73214c0d678578f7dad84268e2e
richlay <lsps950204@gmail.com>
2018-09-16 01:37:26 +0800

    ??

--- week_1/hw_2.R
+++ week_1/hw_2.R
@@ -6,24 +6,35 @@
 
 # generate random four nums
 random_four <- sample(0:9, 4, replace = T, prob = NULL)
+a <- F
+playround <- 1
+while (a==F) {
 # player input four digit
 guess <- as.numeric(readline(prompt = "請輸入四個數字"))
 # get individual nums
 indi_num <- c(1:4)
 a <- 0
 b <- 0
-countb <- c(1:10)
 for (i in 1:4) {
   indi_num[i] <- as.numeric(substr(guess, i, i))
   if(indi_num[i]==random_four[i]){
     a <- a+1
   }else{
-    countb[indi_num[i]] <- countb[indi_num[i]] + 1
+    k <- grep(pattern = indi_num[i], random_four)
+    if(names(random_four[k])!="gg"){
+      if(k>0){
+        b <- b+1
+        names(random_four[k]) <- "gg"
+      }
+    }
   }
 }
 if(random_four==indi_num){
-  print(paste("答對了，正確答案是", random_four))
+  print(paste("答對了，正確答案是 ", random_four, " 你玩了 ", playround, " 次"))
+  a <- T
 }else{
-  
+  print(paste(a,"A",b,"B"))
+  playround <- playround + 1
+}
 }
 # 額外功能：每次玩家輸入完四個數字後，檢查玩家的輸入是否正確(錯誤檢查)
