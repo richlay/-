f36a5bb420fe3e97a72cfa493a290b2bd6e7e591
richlay <43267121+richlay@users.noreply.github.com>
2018-09-20 12:14:48 +0800

    update for bug

--- week_2/class.R
+++ week_2/class.R
@@ -3,7 +3,7 @@ html <- read_html("https://www.michaeljgrogan.com/rvest-web-scraping-using-r/")
 
 #Include relevant HTML nodes using CSS generator:
 marketingtable <- html_nodes(html, ".odd .column-4 , .odd .column-3 , .odd .column-2 , .odd .column-1, .even .column-4 , .even .column-3 , .even .column-2 , .even .column-1")
-
+marketingtable
 #Determine table length
 length(marketingtable)
 
@@ -11,9 +11,6 @@ length(marketingtable)
 webtable <-html_text(marketingtable)
 
 webtable
-
-#Import table from web page
-html <- read_html("https://www.michaeljgrogan.com/rvest-web-scraping-using-r/")
 
 #Structure separate variables according to node
 observation <- html_nodes(html, ".odd .column-1, .even .column-1")
@@ -24,11 +21,15 @@ consumerrating <- html_nodes(html, ".odd .column-4, .even .column-4")
 #Define separate variables
 observationvalues<-html_text(observation)
 marketingspendvalues<-html_text(marketingspend)
+marketingspendvalues
 numberofcampaignsvalues<-html_text(numberofcampaigns)
 consumerratingvalues<-html_text(consumerrating)
 consumerratingvalues
 
 #Structure data frame and remove heading
 df = data.frame(observationvalues, marketingspendvalues, numberofcampaignsvalues, consumerratingvalues)
-df2<-df[-1, ]
-df2
+df2<-df[1:6, ]
+View(df2)
+df3<-df[2, ]
+View(df3)
+
