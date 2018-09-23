48c1de5655b4d92fb66b2cc90afd1c0e2259e000
richlay <43267121+richlay@users.noreply.github.com>
2018-09-24 02:17:34 +0800

    test1 for week1 hw2 ok

--- week_1/hw_2.R
+++ week_1/hw_2.R
@@ -4,37 +4,51 @@
 # 2. 玩家可"重覆"猜電腦所產生的數字，並提示猜測的結果(EX:1A2B)
 # 3. 一旦猜對，系統可自動計算玩家猜測的次數
 
+# some constants
+endofgame <- F
+guess.num <- 1
+already.a <- c(F, F, F, F)
+playround.count <- 0
 # generate random four nums
-random_four <- sample(0:9, 4, replace = T, prob = NULL)
-a <- F
-playround <- 1
-while (a==F) {
-# player input four digit
-guess <- as.numeric(readline(prompt = "請輸入四個數字"))
-# get individual nums
-indi_num <- c(1:4)
-a <- 0
-b <- 0
-for (i in 1:4) {
-  indi_num[i] <- as.numeric(substr(guess, i, i))
-  if(indi_num[i]==random_four[i]){
-    a <- a+1
-  }else{
-    k <- grep(pattern = indi_num[i], random_four)
-    if(names(random_four[k])!="gg"){
-      if(k>0){
-        b <- b+1
-        names(random_four[k]) <- "gg"
+random.four <- sample(0:9, 4, replace = T, prob = NULL)
+
+while (endofgame==F) {
+  a <- 0
+  b <- 0
+  # reset checking number
+  checking.num <- random.four
+  # player input four digit
+  guess <- readline(prompt = "請輸入四個數字")
+  # get individual nums
+  for (i in 1:4) {
+    guess.num[i] <- as.integer(substr(guess, i, i))
+    # find a
+    if (guess.num[i]==random.four[i]){
+      a <- a+1
+      already.a[i] <- T
+    }
+  }
+  
+  for (j in 1:4) {
+    if (already.a[j]==F){
+      for (k in 1:4) {
+        if (guess.num[j]==checking.num[k]){
+          b <- b+1
+          checking.num[k] <- 10
+          break
+        }
       }
     }
   }
-}
-if(random_four==indi_num){
-  print(paste("答對了，正確答案是 ", random_four, " 你玩了 ", playround, " 次"))
-  a <- T
-}else{
-  print(paste(a,"A",b,"B"))
-  playround <- playround + 1
-}
+  
+  if (a==4){
+    print(paste("Correct! You have tried ",playround.count," times."))
+    endofgame <- T
+  }else{
+    print(paste0(a,"A",b,"B"))
+    playround.count <- playround.count+1
+  }
+  
 }
 # 額外功能：每次玩家輸入完四個數字後，檢查玩家的輸入是否正確(錯誤檢查)
+
