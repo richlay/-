d10b7e27888b8a3450d015a10e80567d243e4ed5
richlay <43267121+richlay@users.noreply.github.com>
2018-10-02 15:12:43 +0800

    hw3 rmd created

--- week_3/hw/insurancedata.R
+++ week_3/hw/insurancedata.R
@@ -1,9 +1,7 @@
 setwd("C:/Users/user/Documents/GitHub/Rlanguage/week_3/hw")
 insurance.data = read.csv("insurance.csv")
-male = insurance.data[insurance.data$sex=="male",]
-require(datasets)
-plot(x=male$age,
-     y=male$bmi,
-     main="age to bmi for male",
-     xlab="Age",
-     ylab="BMI")
+
+library(ggplot2)
+my.plot = ggplot(insurance.data, aes(age, bmi, colour=sex)) + geom_point()
+my.plot
+
